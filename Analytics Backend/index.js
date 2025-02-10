ca53dfdcdf647a798d496cbcd13a1a477ef2c705
ashish-allatone <ashish.s@allatone.in>
2025-02-10 23:25:59 +0530

    update index.html

--- Analytics Backend/index.js
+++ Analytics Backend/index.js
@@ -4,7 +4,7 @@ const qs = require("qs");
 const cors = require("cors");
 
 const app = express();
-const port = 8100;
+const port = 3000;
 
 app.use(cors());
 
@@ -59,5 +59,5 @@ app.get("/token", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://140.245.7.177:${port}`);
+  console.log(`Server running at https://slanalytics.mjunction.in:${port}`);
 });
